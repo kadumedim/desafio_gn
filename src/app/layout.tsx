126f70215c498c56eebc2c1c3f5c1cc7fe196724
“kadumedim” <kadumedim123@gmail.com>
2025-02-25 23:44:49 -0300

    feat: initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Space_Grotesk } from "next/font/google";
+import "./globals.css";
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  variable: "--font-space-grotesk",
+});
+
+export const metadata: Metadata = {
+  title: "Desafio GN",
+  description: "Lista de comandos",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br">
+      <body className={`${spaceGrotesk.variable} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image"
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[200px_1fr_20px] items-start justify-items-center min-h-screen sm:p-20 pt-12 font-[family-name:var(--font-space-grotesk)]">
+      <div className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        teste
+      </div>
+    </div>
+  )
+}
